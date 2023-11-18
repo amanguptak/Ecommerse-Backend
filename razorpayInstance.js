74f4cf0b64bf7e89ddaef33da6c773c878e3f5bc
Aman Gupta <eagleax7@gmail.com>
2023-11-18 12:38:25 +0530

    payment route fixed

--- razorpayInstance.js
+++ razorpayInstance.js
@@ -1,13 +1,13 @@
 const Razorpay = require('razorpay')
 const dotenv = require("dotenv")
 
-// const cors = require('cors')
+const cors = require('cors')
 
 dotenv.config({path:"config/config.env"})
-// app.use(cors({
-//     origin:"http://localhost:8080",
-//     credentials:true,
-// }))
+app.use(cors({
+    origin:"https://animeuchicha.vercel.app",
+    credentials:true,
+}))
 
 exports.instance = new Razorpay({
     key_id: process.env.ROZAR_API_KEY,
