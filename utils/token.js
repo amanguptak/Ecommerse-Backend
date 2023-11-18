aae5ffbac4cbbdec0894beb727d81805026a8489
Aman Gupta <eagleax7@gmail.com>
2023-11-18 10:49:26 +0530

    backend-v1

---
+++ utils/token.js
@@ -0,0 +1,21 @@
+//here we get a token and store into cookie not in localStorage
+const sendToken = (user,statusCode,res)=>{
+    const token = user.getJWToken()
+
+    //for cookies
+    const options  = {
+        expires: new Date(
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: false,
+        sameSite:'None',
+        secure:true
+    }
+    res.status(statusCode).cookie("token",token,options ).json({
+        success: true,
+        user,
+        token,
+    })
+}
+
+module.exports = sendToken;
